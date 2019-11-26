8e7134f80a535eddb47cf6ef26fde27779a860c5
zhaofeng-shu33 <616545598@qq.com>
2019-11-26 10:50:49 +0800

    fix plot bug

--- simulation_task_2.py
+++ simulation_task_2.py
@@ -2,8 +2,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import math
-MAX_N = 7
-MAX_INTERVAL = 1
+MAX_N = 11
+MAX_INTERVAL = 3
 def doublefactorial(n): 
   
     if (n == 0 or n == 1): 
@@ -14,8 +14,10 @@ def get_y(x_arr, m):
     scale_factor = np.sqrt(math.factorial(m))
     y_arr = np.zeros(x_arr.shape)
     for i in range(1, m + 1, 2):
-        y_arr += np.power(x_arr, m) * pow(-1, (m-i)/2) / (math.factorial(i) * doublefactorial(m - i))
+        y_arr += np.power(x_arr, i) * np.power(-1, (m - i)/2) / (math.factorial(i) * doublefactorial(m - i))
     y_arr *= scale_factor
+    if(y_arr[50 * MAX_INTERVAL + 2] < 0):
+        y_arr = - y_arr
     return y_arr
 if __name__ == '__main__':
     x = np.linspace(-MAX_INTERVAL, MAX_INTERVAL, 100 * MAX_INTERVAL)
